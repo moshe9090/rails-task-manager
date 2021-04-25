9243fb4874e481ba94dd0ea247f0c16831aaa1bf
Moshe Maman <moshemaman20@gmail.com>
2021-04-25 14:37:09 +0300

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  getv"tasks", to: "task#index"
+  getv "tasks/:id/edit", to: "task#index"
+  patch "tasks/:id", to: "tasks#update"
+
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  delete "tasks/:id", to: "tasks#destory"
+
 end
